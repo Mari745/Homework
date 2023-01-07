3cf30cde50c31d44c6406cdf3550bb978ea0d19c
Maria <maffulia@gmail.com>
2023-01-07 16:50:55 +0300

    Добавлен код к 4 задаче

--- Work4/Program.cs
+++ Work4/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int n = 1;
+while (n <= N)
+{
+    if(n % 2==0)
+
+    Console.WriteLine(n + ", ");
+    n ++;
+}
