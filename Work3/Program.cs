9dff458af893e1af258c3c1d740659beb4e5d1b4
Maria <maffulia@gmail.com>
2023-01-07 16:40:24 +0300

    Добавлен код к задаче 3

--- Work3/Program.cs
+++ Work3/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a;
+Console.WriteLine("Введите число:");
+a=Convert.ToInt32(Console.ReadLine()) ;
+if (a % 2 == 0)
+{
+   Console.WriteLine("{0} четное", a );
+}
+else
+{
+    Console.WriteLine("{0} не четное", a);
+}
+Console.ReadLine();
+
