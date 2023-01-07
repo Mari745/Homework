846db984e12fb8ca0d1744eb10fa0ce8904b295f
Maria <maffulia@gmail.com>
2023-01-07 16:19:03 +0300

    Добавлены код и блок-схема

---
+++ Work1/Program.cs
@@ -0,0 +1,17 @@
+﻿int a, b ;
+Console.WriteLine("Введите два числа:");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+
+if ( a > b )
+{
+   Console.WriteLine("{0} максимальное", a);
+   Console.WriteLine("{0} минимальное" , b);
+}
+
+else
+{
+   Console.WriteLine("{0} минимальное", b);
+   Console.WriteLine("{0} максимальное", a);
+}
+Console.ReadLine();
