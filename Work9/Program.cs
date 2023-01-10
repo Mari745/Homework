5c4eb1a4119699fa7d3ece5a2210727497f0a6fd
Maria <maffulia@gmail.com>
2023-01-10 09:13:16 +0300

    Добавлен код к 9 задаче

---
+++ Work9/Program.cs
@@ -0,0 +1,21 @@
+﻿double GetDistance(double x1,double x2, double y1,
+double y2,double z1,double z2)
+{
+    return Math.Sqrt (Math.Pow(x1-x2,2)
+                    + Math.Pow(y1-y2,2)
+                    + Math.Pow(z1-z2,2));
+
+}
+ double GetDistance(double x1,double x2, double y1,
+double y2,double z1,double z2)
+double GetValue(string text)
+
+double x1=GetValue("x1=");
+double x2=GetValue("x2=");
+double y1=GetValue("y1=");
+double y2=GetValue("y2=");
+double z1=GetValue("z1=");
+double z2=GetValue("z2=");
+
+double distance = GetDistance(x1,x2,y1,y2,z1,z2);
+System.Console.WriteLine(distance);
