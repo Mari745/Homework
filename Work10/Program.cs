67a694301714c1d40ea7caf9a66ad90d2fd63884
Maria <maffulia@gmail.com>
2023-01-10 15:46:23 +0300

    Добавлена корректировка в задачу 9 и блок-схему

--- Work10/Program.cs
+++ Work10/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = ReadInt("Введите число N: ");
+
+for (int i = 1; i <= number; i++)
+{ 
+    Console.Write($"{i*i*i} ");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
